aaf225fef1f83dfb7603fdae0d27197fbdc672d2
Gengler22 <78129491+Gengler22@users.noreply.github.com>
2023-04-01 17:58:40 -0400

    Update agencyBarchart.js

--- js/agencyBarchart.js
+++ js/agencyBarchart.js
@@ -1,210 +1,242 @@
-class agencyBarchart {
+class LeafletMap {
+
     /**
-    * Class constructor with basic chart configuration
-    * @param {Object}
-    * @param {Array}
-    */
+     * Class constructor with basic configuration
+     * @param {Object}
+     * @param {Array}
+     */
     constructor(_config, _data) {
-        // Configuration object with defaults
-        // Important: depending on your vis and the type of interactivity you need
-        // you might want to use getter and setter methods for individual attributes
         this.config = {
             parentElement: _config.parentElement,
-            containerWidth: _config.containerWidth || 1200,
-            containerHeight: _config.containerHeight || 600,
-            margin: _config.margin || { top: 20, right: 40, bottom: 50, left: 75 },
-            tooltipPadding: _config.tooltipPadding || 15
         }
         this.data = _data;
         this.initVis();
     }
 
+    /**
+     * We initialize scales/axes and append static elements, such as axis titles.
+     */
     initVis() {
 
         let vis = this;
 
-        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+        //ESRI
+        vis.esriUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+        vis.esriAttr = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
 
-        for (let i = 0; i < vis.data.length; i++) {
-            if (vis.data[i].agency_responsible != "Public Services" && vis.data[i].agency_responsible != "Cinc Building Dept"
-                && vis.data[i].agency_responsible != "City Manager's Office" && vis.data[i].agency_responsible != "Police Department"
-                && vis.data[i].agency_responsible != "Cinc Health Dept" && vis.data[i].agency_responsible != "Park Department"
-                && vis.data[i].agency_responsible != "Cin Water Works" && vis.data[i].agency_responsible != "Fire Dept"
-                && vis.data[i].agency_responsible != "Metropolitan Sewer" && vis.data[i].agency_responsible != "Dept of Trans and Eng") {
-                vis.data[i].agency_responsible = "Other";
+        //TOPO
+        vis.topoUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
+        vis.topoAttr = 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+
+        //Thunderforest Outdoors- requires key... so meh... 
+        vis.thOutUrl = 'https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey={apikey}';
+        vis.thOutAttr = '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+        //Stamen Terrain
+        vis.stUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}';
+        vis.stAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+        //Defining additional map backgrounds 
+        var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        });
+        var Thunderforest_MobileAtlas = L.tileLayer('https://{s}.tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey={apikey}', {
+            attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            apikey: '27d5774d815b4b54933de70941dd2b8f',
+            maxZoom: 22
+        });
+        var Thunderforest_SpinalMap = L.tileLayer('https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png?apikey={apikey}', {
+            attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            apikey: '27d5774d815b4b54933de70941dd2b8f',
+            maxZoom: 22
+        });
+        var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        });
+        //this is the base map layer, where we are showing the map background
+        vis.base_layer = L.tileLayer(vis.stUrl, {
+            id: 'esri-image',
+            attribution: vis.esriAttr,
+            ext: 'png'
+        });
+
+        vis.zipcodeLayer = L.tileLayer()
+        vis.theMap = L.map('my-map', {
+            center: [39.15, -84.51],
+            zoom: 12,
+            minZoom: 7,
+            maxZoom: 17,
+            layers: [vis.base_layer]
+        });
+
+        //Defining map background names
+        var baseMaps = {
+            "Default Mode": vis.base_layer,
+            "Major Roadways": Thunderforest_MobileAtlas,
+            "Satellite View": Esri_WorldImagery,
+            "Dark Mode": Stadia_AlidadeSmoothDark,
+            "Super Dark Mode": Thunderforest_SpinalMap
+        };
+
+        //Implements map customization GUI
+        var layerControl = L.control.layers(baseMaps).addTo(vis.theMap);
+        //if you stopped here, you would just have a map
+
+        //initialize svg for d3 to add to map
+        L.svg({ clickable: true }).addTo(vis.theMap)// we have to make the svg layer clickable
+        vis.overlay = d3.select(vis.theMap.getPanes().overlayPane)
+        vis.svg = vis.overlay.select('svg').attr("pointer-events", "auto")
+
+        function nodeColor(color) {
+
+            if (color.agency_responsible == "Public Services") {
+                return 'yellow';
+            }
+            else if (color.agency_responsible == "Cinc Building Dept") {
+                return 'orange';
+            }
+            else if (color.agency_responsible == "Police Department") {
+                return 'steelblue';
+            }
+            else if (color.agency_responsible == "City Manager's Office") {
+                return 'grey';
+            }
+            else if (color.agency_responsible == "Dept of Trans and Eng") {
+                return 'green';
+            }
+            else if (color.agency_responsible == "Cinc Health Dept") {
+                return '#EF5350';
+            }
+            else if (color.agency_responsible == "Cin Water Works") {
+                return 'blue';
+            }
+            else if (color.agency_responsible == "Park Department") {
+                return '#81C784';
+            }
+            else if (color.agency_responsible == "Fire Dept") {
+                return '#B71C1C';
+            }
+            else if (color.agency_responsible == "Metropolitan Sewer") {
+                return 'darkgreen'
+            }
+            else {
+                return '#5E35B1';
             };
         };
 
-        vis.xScale = d3.scaleBand()
-            .range([0, vis.width])
-            .paddingInner(0.15)
-            .padding(0.2);
+        //handler here for updating the map, as you zoom in and out           
+        vis.theMap.on("zoomend", function () {
+            vis.updateVis();
+        });
 
-        vis.yScale = d3.scaleLinear()
-            .range([vis.height, 0]);
-
-        // Initialize axes
-        vis.xAxis = d3.axisBottom(vis.xScale)
-            .tickSizeOuter(0);
-
-
-
-        vis.yAxis = d3.axisLeft(vis.yScale)
-            .scale(vis.yScale)
-            .tickSizeOuter(0)
-            .ticks(11);
-
-        // Define size of SVG drawing area
-        vis.svg = d3.select(vis.config.parentElement)
-            .attr('width', vis.config.containerWidth)
-            .attr('height', vis.config.containerHeight);
-
-        vis.svg.append("text")
-            .attr("transform", "translate(0,0)")
-            .attr("x", 550)
-            .attr("y", 20)
-            .attr("font-size", "14px")
-            .text("Number of Calls for each Agency Responsible")
-
-
-
-        // Append group element that will contain our actual chart 
-        // and position it according to the given margin config
-        vis.chart = vis.svg.append('g')
-            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
-
-        // Append empty x-axis group and move it to the bottom of the chart
-        vis.xAxisG = vis.chart.append('g')
-            .attr('class', 'axis x-axis')
-            .attr('transform', `translate(0,${vis.height})`);
-
-
-        // Append y-axis group 
-        vis.yAxisG = vis.chart.append('g')
-            .attr('class', 'axis y-axis')
-
-
-
-        vis.updateVis();
-
-        // Append titles, legends and other static elements here
-        // ...
     }
 
-    /**
-     * This function contains all the code to prepare the data before we render it.
-     * In some cases, you may not need this function but when you create more complex visualizations
-     * you will probably want to organize your code in multiple functions.
-     */
     updateVis() {
         let vis = this;
 
-        // Set the scale input domains
+        function nodeColor(color) {
 
-        vis.xAxisG.selectAll('g')
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.5em")
-            .attr("dy", ".4em")
-            .attr("font-size", "9px")
-            .attr("transform", "rotate(-30)");
+            if (color.agency_responsible == "Public Services") {
+                return 'yellow';
+            }
+            else if (color.agency_responsible == "Cinc Building Dept") {
+                return 'orange';
+            }
+            else if (color.agency_responsible == "Police Department") {
+                return 'steelblue';
+            }
+            else if (color.agency_responsible == "City Manager's Office") {
+                return 'grey';
+            }
+            else if (color.agency_responsible == "Dept of Trans and Eng") {
+                return 'green';
+            }
+            else if (color.agency_responsible == "Cinc Health Dept") {
+                return '#EF5350';
+            }
+            else if (color.agency_responsible == "Cin Water Works") {
+                return 'blue';
+            }
+            else if (color.agency_responsible == "Park Department") {
+                return '#81C784';
+            }
+            else if (color.agency_responsible == "Fire Dept") {
+                return '#B71C1C';
+            }
+            else if (color.agency_responsible == "Metropolitan Sewer") {
+                return 'darkgreen'
+            }
+            else {
+                return '#5E35B1';
+            };
+        };
+
+        //want to see how zoomed in you are? 
+        // console.log(vis.map.getZoom()); //how zoomed am I
+
+        //want to control the size of the radius to be a certain number of meters? 
+        vis.radiusSize = 3;
+
+        // if( vis.theMap.getZoom > 15 ){
+        //   metresPerPixel = 40075016.686 * Math.abs(Math.cos(map.getCenter().lat * Math.PI/180)) / Math.pow(2, map.getZoom()+8);
+        //   desiredMetersForPoint = 100; //or the uncertainty measure... =) 
+        //   radiusSize = desiredMetersForPoint / metresPerPixel;
+        // }
 
 
-        vis.xAxisG.append('text')
-            .attr("transform", "translate(0,0)")
-            .attr("y", vis.height - 495)
-            .attr("x", vis.width - 550)
-            .attr("font-size", "16px")
+
+        //redraw based on new zoom- need to recalculate on-screen position
+        vis.Dots = vis.svg.selectAll('circle')
+            .data(vis.data)
+            .join('circle')
+            .attr("fill", nodeColor)
             .attr("stroke", "black")
-            .text("Agency Responsible");
+            //Leaflet has to take control of projecting points. Here we are feeding the latitude and longitude coordinates to
+            //leaflet so that it can project them on the coordinates of the view. Notice, we have to reverse lat and lon.
+            //Finally, the returned conversion produces an x and y point. We have to select the the desired one using .x or .y
+            .attr("cx", d => vis.theMap.latLngToLayerPoint([d.latitude, d.longitude]).x)
+            .attr("cy", d => vis.theMap.latLngToLayerPoint([d.latitude, d.longitude]).y)
+            .attr("r", vis.radiusSize)
+            .on('mouseover', function (event, d) { //function to add mouseover event
+                d3.select(this).transition() //D3 selects the object we have moused over in order to perform operations on it
+                    .duration('150') //how long we are transitioning between the two states (works like keyframes)
+                    .attr("fill", "red") //change the fill
+                    .attr('r', 4); //change radius
 
-        vis.yAxisG.append('text')
-            .attr("transform", "rotate(-90)")
-            .attr("dy", "-13.5em")
-            .attr("y", vis.height - 430)
-            .attr("x", vis.width - 1300)
-            .attr("font-size", "12px")
-            .attr("stroke", "black")
-            .text("Number of Calls");
+                //create a tool tip
+                d3.select('#tooltip')
+                    .style('opacity', 1)
+                    .style('z-index', 1000000)
+                    // Format number with million and thousand separator
+                    .html(`<div class="tooltip-label"><h3>Service ID: ${d.service_request_iD}</h3> <li>Call type: ${d.service_name} </li> 
+                    <li>Request Date: ${d.requested_datetime}</li> <li> Updated Date: ${d.updated_datetime}</li> <li> Public agency: ${d.agency_responsible} </li>
+                    <li> Description: ${d.description} </li> </div>`);
 
-        const orderedKeys = ['Cinc Building Dept', 'Cinc Health Dept', 'Dept of Trans and Eng', 'Police Department', 'Public Services', 'Park Department', 'Cin Water Works', 'Fire Dept', 'Metropolitan Sewer', 'Other'];
+            })
+            .on('mousemove', (event) => {
+                //position the tooltip
+                d3.select('#tooltip')
+                    .style('left', (event.pageX + 10) + 'px')
+                    .style('top', (event.pageY + 10) + 'px');
+            })
+            .on('mouseleave', function () { //function to add mouseover event
+                d3.select(this).transition() //D3 selects the object we have moused over in order to perform operations on it
+                    .duration('150') //how long we are transitioning between the two states (works like keyframes)
+                    .attr("fill", nodeColor) //change the fill
+                    .attr('r', 3) //change radius
 
-        let agency_responsible = d3.rollups(vis.data, v => v.length, d => d.agency_responsible);
-        let swap = agency_responsible[10];
-        agency_responsible[10] = agency_responsible[6];
-        agency_responsible[6] = swap;
-        console.log(agency_responsible);
-        const aggregatedData = Array.from(agency_responsible, ([key, count]) => ({ key, count }));
+                d3.select('#tooltip').style('opacity', 0);//turn off the tooltip
 
-        // Filter the aggregatedData array to only include keys in the orderedKeys array
-        vis.aggregatedData = aggregatedData.filter(d => orderedKeys.includes(d.key));
-
-        vis.xValue = d => d.key;
-        vis.yValue = d => d.count;
-
-        vis.xScale.domain(vis.aggregatedData.map(vis.xValue));
-        vis.yScale.domain([1, d3.max(vis.aggregatedData, vis.yValue) + 500]);
-
-        vis.renderVis();
+            })
 
     }
 
-    /**
-     * This function contains the D3 code for binding data to visual elements.
-     * We call this function every time the data or configurations change 
-     * (i.e., user selects a different year)
-     */
+
     renderVis() {
         let vis = this;
 
-        vis.xAxisG.selectAll('g')
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.5em")
-            .attr("dy", ".4em")
-            .attr("font-size", "9px")
-            .attr("transform", "rotate(-30)");
+        //not using right now... 
 
-        // Add rectangles
-        const bars = vis.chart.selectAll('.bar')
-            .data(vis.aggregatedData, d => d.agency_responsible)
-            .join('rect')
-            .attr('class', 'bar')
-            .attr('width', vis.xScale.bandwidth())
-            .attr('height', d => vis.height - vis.yScale(vis.yValue(d)))
-            .attr('y', d => vis.yScale(vis.yValue(d)))
-            .attr('x', d => vis.xScale(vis.xValue(d)))
-            .style('opacity', 0.5)
-            .style('opacity', 1)
-            .style('fill', "#8AAC80") //change me 
-            .on('mouseover', (event, d) => {
-                d3.select('#tooltip1')
-                    .style('display', 'block')
-                    .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')
-                    .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
-                    .style('opacity', 1)
-                    // Format number with million and thousand separator
-                    .html(`<div class="tooltip-title2">Agency Responsible</div><ul>${d3.format(',')(d.count)} </ul>`);
-            })
-            .on('mouseleave', () => {
-                d3.select('#tooltip1').style('display', 'none');
-            })
-            .on('click', function (event, d) {
-                const isActive = agencyFilter.includes(d.key);
-                if (isActive) {
-                    agencyFilter = agencyFilter.filter(f => f !== d.key); // Remove filter
-                } else {
-                    agencyFilter.push(d.key); // Append filter
-                }
-                AgencyFilter(); // Call global function to update scatter plot
-                d3.select(this).classed('active', !isActive); // Add class to style active filters with CSS
-            });
-
-        // Update the axes because the underlying scales might have changed
-        vis.xAxisG.call(vis.xAxis);
-        vis.yAxisG.call(vis.yAxis);
     }
-};
-
-
+}
